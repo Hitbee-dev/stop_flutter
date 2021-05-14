74ca0c25d6a6d7b336041331e195cd07389e1a84
Hitbee-dev <ckim.esw@gmail.com>
2021-05-14 11:03:10 +0900

    킥보드 아이콘 marker 추가

--- stop_app/lib/google_maps.dart
+++ stop_app/lib/google_maps.dart
@@ -1,8 +1,22 @@
 import 'dart:async';
-
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:stop_app/informations.dart';
+
+Future<Uint8List> getBytesFromAsset(String path, int width) async {
+  ByteData data = await rootBundle.load(path);
+  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+  ui.FrameInfo fi = await codec.getNextFrame();
+  return (await fi.image.toByteData(format: ui.ImageByteFormat.png)).buffer.asUint8List();
+}
+
+Future<BitmapDescriptor> getBitmapDescriptorFromAssetBytes(String path, int width) async {
+  final Uint8List imageData = await getBytesFromAsset(path, width);
+  return BitmapDescriptor.fromBytes(imageData);
+}
 
 class GoogleMaps extends StatefulWidget {
   @override
@@ -11,25 +25,43 @@ class GoogleMaps extends StatefulWidget {
 }
 
 class GoogleMapsState extends State<GoogleMaps> {
+  Set<Marker> _markers = {};
+  BitmapDescriptor mapMarker;
 
   @override
-  initState(){
+  void initState(){
     super.initState();
-
+    setCustomMarker();
   }
 
-  Completer<GoogleMapController> _controller = Completer();
+  void setCustomMarker() async {
+    mapMarker = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(),
+        "assets/images/kickboard_icon.png");
+  }
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(36.3544591, 127.4189559),
-    zoom: 14.4746,
-  );
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _markers.add(
+        Marker(
+            markerId: MarkerId('1'),
+            position: LatLng(36.3544591, 127.4189559),
+            icon: mapMarker,
+            infoWindow: InfoWindow(
+              title: "한남대학교 공과대학",
+              snippet: "CU"
+          )
+        )
+      );
+    });
+  }
 
-  static final CameraPosition _kLake = CameraPosition(
+  static final CameraPosition initialLocation = CameraPosition(
       // bearing: 192.8334901395799,
       // tilt: 59.440717697143555,
       target: LatLng(36.3544591, 127.4189559),
-      zoom: 19.151926040649414);
+      zoom: 16,
+  );
 
   String qrcode_name = "QR SCAN";
   String qrcode_name1 = "운행 종료";
@@ -37,15 +69,15 @@ class GoogleMapsState extends State<GoogleMaps> {
   int qrIndex = 0;
 
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+        initialCameraPosition: initialLocation,
+        markers: _markers,
+        onMapCreated: _onMapCreated
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _qrChange,
